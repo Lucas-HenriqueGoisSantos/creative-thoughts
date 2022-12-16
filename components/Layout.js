eb1655320c4710588261ca748e5f3526da921255
Lucas-HenriqueGoisSantos <89606089+Lucas-HenriqueGoisSantos@users.noreply.github.com>
2022-12-15 22:47:35 -0300

    Changes to be committed: 	modified:   components/Layout.js 	modified:   package-lock.json 	modified:   package.json 	modified:   pages/auth/login.js

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,10 @@
 import Nav from './Nav';
 
 export default function Layout({ children }){
-    return(
-        <div className="mx-6 md:max-w-2x1 font-poppins">
-            <Nav />
-            <main>{children}</main>
-        </div>
-    );
+	return(
+		<div className="mx-6 md:max-w-2xl font-poppins">
+			<Nav />
+			<main>{children}</main>
+		</div>
+	);
 }
