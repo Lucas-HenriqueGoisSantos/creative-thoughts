a6a37cbb6b610c835537e956cefa0e8c83706962
Lucas-HenriqueGoisSantos <89606089+Lucas-HenriqueGoisSantos@users.noreply.github.com>
2022-12-22 23:28:51 -0300

    Changes to be committed: 	modified:   components/Layout.js 	modified:   components/Nav.js

--- components/Nav.js
+++ components/Nav.js
@@ -4,6 +4,7 @@ import { useAuthState } from "react-firebase-hooks/auth";
 
 export default function Nav(){
   const [user, loading] = useAuthState(auth);
+  console.log(user);
   return (
     <nav className="flex justify-between items-center py-10">
       <Link href="/">
@@ -14,15 +15,15 @@ export default function Nav(){
           <Link href={ "/auth/login" } className="py-2 px-4 text-sm bg-cyan-500 text-white rounded-lg font-medium ml-8"> Join Now </Link>
           )}
         {user && (
-          <div>
+          <div className="flex items-center gap-6">
             <Link href='/post'>
-              <button>post</button>
+              <button className="font-medium bg-cyan-500 text-white py-2 px-4 rounded-md text-sm">Post</button>
             </Link>
             <Link href='/dashboard'>
-              <h1>Dashboard</h1>
+              <img className="w-12 rounded-full cursor-pointer" src={user.photoURL} />
             </Link>
           </div>
-          )};
+          )}
       </ul>
     </nav>
   );
