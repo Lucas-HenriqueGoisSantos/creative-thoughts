95e336f82ddc57a18b53ed587d0c81c6ec4a8435
Lucas-HenriqueGoisSantos <89606089+Lucas-HenriqueGoisSantos@users.noreply.github.com>
2022-12-25 19:44:21 -0300

    Changes to be committed: 	modified:   pages/post.js

--- pages/post.js
+++ pages/post.js
@@ -2,3 +2,19 @@ import { auth, db } from "../utils/firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+
+export default function Post() {
+  return (
+    <div className="">
+      <form>
+        <h1>Create a new post</h1>
+        <div>
+          <h3>Description</h3>
+          <textarea></textarea>
+          <p>0/300</p>
+        </div>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
