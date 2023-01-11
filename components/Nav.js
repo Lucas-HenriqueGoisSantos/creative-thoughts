f8c29302d79d15ad1d4dae239843fb73be8a7850
Lucas-HenriqueGoisSantos <89606089+Lucas-HenriqueGoisSantos@users.noreply.github.com>
2023-01-11 18:19:29 -0300

    Changes to be committed: 	modified:   components/Nav.js 	new file:   components/message.js 	modified:   pages/dashboard.js 	modified:   pages/index.js 	modified:   pages/post.js

--- components/Nav.js
+++ components/Nav.js
@@ -4,24 +4,36 @@ import { useAuthState } from "react-firebase-hooks/auth";
 
 export default function Nav(){
   const [user, loading] = useAuthState(auth);
-  console.log(user);
+  
   return (
     <nav className="flex justify-between items-center py-10">
       <Link href="/">
         <button className="text-lg font-medium">Creative Thoughts</button>
       </Link>
       <ul className="flex items-center gap-10">
-        {!user &&(
-          <Link href={ "/auth/login" } className="py-2 px-4 text-sm bg-cyan-500 text-white rounded-lg font-medium ml-8"> Join Now </Link>
+        {!user && (
+          <Link
+            href={"/auth/login"}
+            className="py-2 px-4 text-sm bg-cyan-500 text-white rounded-lg font-medium ml-8"
+          >
+            {" "}
+            Join Now{" "}
+          </Link>
         )}
 
         {user && (
           <div className="flex items-center gap-6">
-            <Link href='/post'>
-              <button className="font-medium bg-cyan-500 text-white py-2 px-4 rounded-md text-sm">Post</button>
+            <Link href="/post">
+              <button className="font-medium bg-cyan-500 text-white py-2 px-4 rounded-md text-sm">
+                Post
+              </button>
             </Link>
-            <Link href='/dashboard'>
-              <img className="w-12 rounded-full cursor-pointer" src={user.photoURL} />
+            <Link href="/dashboard">
+              <img
+                className="w-12 rounded-full cursor-pointer"
+                rel="noreferrer"
+                src={user.photoURL}
+              />
             </Link>
           </div>
         )}
